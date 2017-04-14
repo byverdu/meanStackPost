f7962179160fb6a8f4ff4b76a00aa4594557a0ea
albert Byverdu :) <byverdu@gmail.com>
2017-04-14 16:21:19 +0100

    changed if/else by promises

--- test/serverSpec.js
+++ test/serverSpec.js
@@ -1,35 +1,27 @@
+/* eslint-disable import/no-extraneous-dependencies*/
+// Test cases for routing
+
+import chai from 'chai';
+import request from 'supertest';
+
 import server from '../server';
 
-const request = require('supertest'); // eslint-disable-line
+const expect = chai.expect;
 
 describe( 'Root path', () => {
-  it( 'Visiting the root path should return 200', ( done ) => {
+  it( 'Visiting the root path should return 200', () => {
     request( server )
       .get( '/' )
       .expect( 200 )
-      .expect( 'Up and running' )
-      .end(( err, res ) => {
-        if ( err ) {
-          done( err );
-        } else {
-          done();
-        }
-      });
+      .then( response => expect( response.text ).to.equal( 'Up and running' ));
   });
 });
 
 describe( 'Not Found path', () => {
-  it( 'Visiting a not known route should return 404', ( done ) => {
+  it( 'Visiting a not known route should return 404', () => {
     request( server )
       .get( '/notFound' )
       .expect( 404 )
-      .expect( 'Sorry route not found' )
-      .end(( err, res ) => {
-        if ( err ) {
-          done( err );
-        } else {
-          done();
-        }
-      });
+      .then( response => expect( response.text ).to.equal( 'Sorry route not found' ));
   });
 });
