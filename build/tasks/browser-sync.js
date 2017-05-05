ea881c725ceed7b4872481b4345d948edce8e419
albert Byverdu :) <byverdu@gmail.com>
2017-05-05 08:08:32 +0100

    added browser sync task

---
+++ build/tasks/browser-sync.js
@@ -0,0 +1,33 @@
+
+/* eslint-disable import/no-extraneous-dependencies*/
+
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+import nodemon from 'gulp-nodemon';
+
+const browserOpts = {
+  proxy: 'http://localhost:3000',
+  port: 9000,
+  open: false
+};
+
+gulp.task( 'browserSync', () => {
+  let restarted = false;
+
+  browserSync.create();
+  browserSync.init( browserOpts );
+
+  return nodemon({
+    exec: 'nodemon --exec  babel-node  ./bin/www'
+  })
+    .on( 'start', () => {
+      if ( restarted ) {
+        setTimeout(() => {
+          browserSync.reload();
+        }, 4000 );
+      }
+    })
+    .on( 'restart', () => {
+      restarted = true;
+    });
+});
